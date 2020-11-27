b932495061074d93bc6fcb475fba80dd35c920ae
Alexandr Krasnov <sanor00@yandex.ru>
2020-11-27 23:32:26 +0300

    add: draft UI main/options menu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+  
+  public string sceneLoadedOnPlay; // Main scene name 
+  public void OnClick_PlayButton() {
+    SceneManager.LoadScene(sceneLoadedOnPlay);
+  }
+
+  public void OnClick_OptionsButton() {
+    // No specific actions, see OptionsButton->Button->OnClick()
+  }
+
+  public void OnClick_CreditsButton() {
+    // No specific actions, see CreditsButton->Button->OnClick()
+  }
+
+  public void OnClick_QuitButton() {
+    Debug.Log("Quit Button Pressed");
+    Application.Quit();
+  }
+}
---
+++ Assets/Scripts/OptionsMenuScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuScript : MonoBehaviour {
+  public void OnClick_BackButton() {
+    // No specific actions, see BackButton->Button->OnClick()
+  }
+}
